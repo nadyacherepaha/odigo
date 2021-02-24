58f0f6e28b35694c0555a94773a50ff27ee7507d
nadyacherepaha <nad.cherepaha@gmail.com>
2021-02-24 21:22:08 +0300

    Add project

---
+++ src/app/app.js
@@ -0,0 +1,9 @@
+import './app.scss';
+
+import $ from 'jquery';
+
+import './components/header-es5.min.js';
+import './components/burger-es5.min.js';
+import './components/scroll-es5.min.js';
+
+window.jQuery = $;
